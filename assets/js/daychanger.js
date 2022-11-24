153e2c82e3a3e00c7b22ced51589ea198ce8084a
Stenoliv <stenbaco@arcada.fi>
2022-11-24 16:13:10 +0200

    Added countdown for time remainign until casino open Fixed casino closing and made it more simple

--- assets/js/daychanger.js
+++ assets/js/daychanger.js
@@ -1,15 +1,11 @@
-document.querySelector(".cm").addEventListener('click', monday)
-{
-    function monday()
-    {
-        mon = new Date()
-        
-        if (mon.getDay()!= 1)
-        {
-            mon.setDate(11);
-            
-        }
-    }
-    console.log(mon.getDay());
-}
 
+setInterval(function()
+{
+    now = new Date();
+    choice = document.querySelector('#days').value
+    
+    if(choice.value == document.querySelector('#mon'))
+    {
+        Set.Date = 1;
+    }
+},10000)
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,56 @@
 
 console.log('main.js init'); // För att se att skriptet laddats in
 //Boolen manual is the main switch for if the casino is open / 0 means it is closed, 1 means it is open:
-let manual = 0;
+let manual = 1;
 var auto = 1;
 let info; 
-close = new Date()
+close = new Date();
 
-setInterval (function wknd()
+document.addEventListener("DOMContentLoaded",function wknd()
 {
     if(close.getDay()=== 6 || close.getDay() === 0 || manual == 0)
     {
-        auto = 0 
-        console.log("if runs")
-        window.open(src = "http://127.0.0.1:5500/pages/closed.html","_self")
+        auto = 0 ;
+        document.querySelector('#makeuser').style.display = 'none';
+        document.querySelector('#nav').style.display = 'none';
+        document.querySelector('#textopen').style.display = 'none'
+        document.querySelector('#textclosed').style.display = 'flex'
     }
     else
     {
-        auto = 1
+        auto = 1;
     }
     console.log(manual);
     console.log(auto);
-},100)
+});
+
+//Time until open
+//calculate time between now and next monday at 00:01
+var open = new Date();
+open.setDate((open.getDate()+(1+7-open.getDay())%7));
+open.setHours(0,1,0,0)
+
+//Convert the time from milliseconds to hh/mm/ss
+setInterval(() => 
+{
+    
+    var now = new Date();
+    
+    let time = open - now;
+
+        let s,m,h;
+        s = Math.floor(time / 1000);
+        m = Math.floor(s/60);
+        h = Math.floor(m/60);
+        
+        s = s % 60;
+        m = m % 60;
+        
+        
+        document.querySelector("#openIn").innerText = h + " Hours " + m + " Minutes " + s + " Seconds ";
+        
+}, 100);
+
     
 //clock
 setInterval(function()
@@ -60,7 +90,7 @@ function submit()
     let use2 = lastname.slice(0, 4);
     let usrn = use2.concat(use1);
     usrn = usrn.toLowerCase();
-    let output = ("Hello " + firstname + " your username is: " + usrn + "Welcome to Razver!");
+    let output = ("Hello " + firstname + " your username is: " + usrn + " Welcome to Razver!");
     document.querySelector('#out').innerText = output;
     }
 };
