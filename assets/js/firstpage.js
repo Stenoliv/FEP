e4c9a43a2ed7d63e76fb96a2c376d314af492059
Stenoliv <stenbaco@arcada.fi>
2022-11-30 19:59:31 +0200

    Home session 2 2022.11.30 adds finished lightbox adds images adds some styling some moving of parts of code to different pages

--- assets/js/firstpage.js
+++ assets/js/firstpage.js
@@ -29,3 +29,53 @@ function submit()
     document.querySelector('#out').innerText = output;
     }
 };
+//Check what day it is and make it active in the daychanger
+document.addEventListener('DOMContentLoaded', when)
+function when()
+{
+    const days = ["0","1","2","3","4","5","6"];
+    for(let i = 0 ; weekend.getDay() != i -1; i++)
+    {
+        function whatDay() 
+        {
+            const day = document.querySelector('#days');
+            day.value = days[i];
+        }
+        whatDay()
+    }
+};
+
+//Changes the day based on selected day in the selector
+
+document.querySelector('#days').addEventListener('change', function cd()
+{
+        document.querySelector('#days').value = weekend.getDay();
+        weekend.getDay;
+})
+
+//Time until open
+//calculate time between now and next monday at 00:01
+var open = new Date();
+open.setDate((open.getDate()+(1+7-open.getDay())%7));
+open.setHours(0,1,0,0)
+
+//Convert the time from milliseconds to hh/mm/ss
+setInterval(() => 
+{
+
+var now = new Date();
+
+let time = open - now;
+
+    let s,m,h;
+    s = Math.floor(time / 1000);
+    m = Math.floor(s/60);
+    h = Math.floor(m/60);
+    
+    s = s % 60;
+    m = m % 60;
+    
+    
+    document.querySelector("#openIn").innerText = h + " Hours " + m + " Minutes " + s + " Seconds ";
+    
+}, 100);
--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,4 +1,19 @@
-document.querySelector(".thumb").forEach((thing)=> 
+document.querySelectorAll(".thumb").forEach((thing)=> 
 {
     console.log(thing);
-})
+
+    thing.addEventListener('click', ( )=> 
+    {
+        const largeimage = thing.getAttribute('data-img');
+        document.querySelector('#largeImage > img').setAttribute('src', largeimage);
+        document.querySelector('#largeImage').style.display = 'flex';
+        document.querySelector('#x').style.display = 'flex';
+    });
+});
+function close()
+{
+    document.querySelector('#largeImage').style.display = 'none';
+    document.querySelector('#x').style.display = 'none';
+}
+
+document.querySelector('#x').addEventListener('click', close);
